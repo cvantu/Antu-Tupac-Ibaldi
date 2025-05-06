4f80917c788b35248ad029f71dae86dee581462b
cvantu <ibaldiantu@gmail.com>
2025-05-06 09:51:33 -0300

    Add files via upload

---
+++ js/algoritmos.js
@@ -0,0 +1,18 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const headers = document.querySelectorAll("h2");
+
+        headers.forEach(header => {
+            header.addEventListener("click", function () {
+                this.classList.toggle("activo"); // Cambia color y fondo
+
+                let next = this.nextElementSibling;
+                while (next && next.tagName !== "H2") {
+                    if (next.tagName === "P") {
+                        next.classList.toggle("oculto");
+                    }
+                    next = next.nextElementSibling;
+                }
+            });
+        });
+    });
