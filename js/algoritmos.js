bf09be7885ee4407c926be0d3ab4bf6c3e25e145
cvantu <ibaldiantu@gmail.com>
2025-05-06 14:44:41 -0300

    Add files via upload

--- js/algoritmos.js
+++ js/algoritmos.js
@@ -1,27 +1,38 @@
-const imagen = document.querySelector("img");
+const headers = document.querySelectorAll("h2");
+const imagen = document.getElementById("imagen");
+const primerH2 = document.getElementById("primerH2");
+let numero= 1
 
 function mostrarVisible(){
-    imagen.classList.remove('imagen-oculta');
-    imagen.classList.add('imagen-visible');
+    numero= ++numero
+    if (numero%2==0)  {
+        imagen.classList.remove('imagen-oculta')
+        imagen.classList.add('imagen-visible')
+
+    } 
+    if (numero%2!=0){
+        imagen.classList.remove('imagen-visible')
+        imagen.classList.add('imagen-oculta')
+
+    }
 }
 
-    
-    
-    document.addEventListener("DOMContentLoaded", function () {
-        const headers = document.querySelectorAll("h2");
+document.addEventListener("DOMContentLoaded", function () {
+   
+    headers.forEach(header => {
+        header.addEventListener("click", function () {
+            this.classList.toggle("activo");
 
-        headers.forEach(header => {
-            header.addEventListener("click", function () {
-                this.classList.toggle("activo"); // Cambia color y fondo
-
-                let next = this.nextElementSibling;
-                while (next && next.tagName !== "H2") {
-                    if (next.tagName === "P") {
-                        next.classList.toggle("oculto");
-                    }
-                    next = next.nextElementSibling;
+            let next = this.nextElementSibling;
+            while (next && next.tagName !== "H2") {
+                if (next.tagName === "P") {
+                    next.classList.toggle("oculto");
                 }
-            });
+                next = next.nextElementSibling;
+            }
+
+        
         });
     });
+});
    
